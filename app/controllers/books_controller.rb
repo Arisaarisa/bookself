6657dbe8b174f7e4df4347a38a98cff4ca315bd3
arisa <arisankochira10@gmail.com>
2019-11-13 23:45:23 +0900

    書籍アプリベースの完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,35 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to @book, notice: "書籍を登録しました。"
+    if @book.save
+      redirect_to @book, notice: "書籍を登録しました。"
+    else
+      render :new
+    end
+
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book, notice: "書籍を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "書籍を削除しました。"
   end
 
   private
